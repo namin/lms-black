1ba6cc961a91414c114085adb03fcb92f499a949
Nada Amin <namin@alum.mit.edu>
2015-08-14 16:12:36 +0200

    added minimal parser, wish i had done this before

---
+++ src/main/scala/lms/black/parser.scala
@@ -0,0 +1,18 @@
+package scala.lms.black
+
+import eval._
+import scala.util.parsing.combinator._
+
+object parser extends JavaTokenParsers with PackratParsers {
+    def exp: Parser[Value] =
+      "#f" ^^ { case _ => B(false) } |
+      "#t" ^^ { case _ => B(true) } |
+      "[0-9]+".r ^^ { case s => I(s.toInt) } |
+      "[a-z]+".r ^^ { case s => S(s) } |
+      "()" ^^ { case _ => N } |
+      "(" ~> exps <~ ")" ^^ { case vs => vs }
+
+  def exps: Parser[Value] =
+      exp ~ exps ^^ { case v~vs => P(v, vs) } |
+      exp ^^ { case v => P(v, N) }
+}
