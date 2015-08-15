3f4e94b4919355213997f4edf0dcb3cb315d0e8d
Nada Amin <namin@alum.mit.edu>
2015-08-15 19:48:21 +0200

    ok

--- src/test/scala/lms/black/repl.scala
+++ src/test/scala/lms/black/repl.scala
@@ -35,4 +35,29 @@ class TestRepl extends TestSuite with BeforeAndAfter {
     assertResult(I(13)){ev("(fib 7)")}
     assertResult(I(0)){ev("(EM counter)")}
   }
+
+  test("app counter") {
+    ev("(EM (define counter 0))")
+    ev("(EM (define old-eval-application eval-application))")
+    ev("""(EM (set! eval-application (clambda (e r k)
+      (set! counter (+ counter 1))
+      (old-eval-application e r k))))""")
+    ev("(define fib (lambda (n) (if (< n 2) n (+ (fib (- n 1)) (fib (- n 2))))))")
+    ev("(EM (set! counter 0))")
+    assertResult(I(13)){ev("(fib 7)")}
+    assertResult(I(142)){ev("(EM counter)")}
+    ev("(set! fib (clambda (n) (if (< n 2) n (+ (fib (- n 1)) (fib (- n 2))))))")
+    ev("(EM (set! counter 0))")
+    assertResult(I(987)){ev("(fib 16)")}
+    assertResult(I(11174)){ev("(EM counter)")}
+    ev("(EM (set! eval-application old-eval-application))")
+    ev("(EM (set! counter 0))")
+    assertResult(I(987)){ev("(fib 16)")}
+    // NB: the recursive calls are not counted!
+    assertResult(I(4789)){ev("(EM counter)")}
+    ev("(set! fib (lambda (n) (if (< n 2) n (+ (fib (- n 1)) (fib (- n 2))))))")
+    ev("(EM (set! counter 0))")
+    assertResult(I(13)){ev("(fib 7)")}
+    assertResult(I(0)){ev("(EM counter)")}
+  }
 }
