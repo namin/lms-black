b2bfe6cfcdd70ead2410491f9ab6f2836d8c625c
Nada Amin <namin@alum.mit.edu>
2016-02-12 09:19:42 +0100

    simple delta call/cc test

---
+++ src/test/scala/lms/black/delta.scala
@@ -0,0 +1,100 @@
+package scala.lms.black
+
+import eval._
+import repl._
+
+import org.scalatest.BeforeAndAfter
+
+class TestDelta extends TestSuite with BeforeAndAfter {
+  val under = "delta_"
+
+  before {
+    clean()
+  }
+
+  def reify_env = """(begin
+(define old-eval-var eval-var)
+
+(set! eval-var
+  (clambda (e r k)
+      (if (eq? '_env e) (k r) (old-eval-var e r k))))
+)"""
+
+  def delta = """(begin
+(define list
+  (lambda args args))
+
+(define old-eval-application eval-application)
+
+(define delta?
+  (lambda (e)
+    (if (pair? e) (if (pair? (car e)) (eq? 'delta (car (car e))) #f) #f)))
+
+(define id-cont
+  (lambda (v) v))
+
+(define make-pairs
+  (lambda (ks vs) 'TODO))
+(set! make-pairs
+  (lambda (ks vs)
+      (if (null? ks) '()
+          (if (null? vs) '()
+              (if (symbol? ks) (cons (cons ks vs) '())
+                  (cons (cons (car ks) (car vs)) (make-pairs (cdr ks) (cdr vs))))))))
+
+(define extend
+  (lambda (env params args)
+      (cons (make-pairs params args) env)))
+
+(define apply-delta
+  (lambda (e r k)
+    (let ((operator (car e))
+          (operand (cdr e)))
+      (let ((delta-params (car (cdr operator)))
+            (delta-body (cdr (cdr operator))))
+        (eval-begin
+         delta-body
+         (extend _env delta-params (list operand r k))
+         id-cont)))))
+
+(set! eval-application
+      (lambda (e r k)
+        (if (delta? e)
+            (apply-delta e r k)
+            (old-eval-application e r k))))
+
+(define meaning base-eval)
+)"""
+
+  def call_cc = """(begin
+(define call/cc
+  (lambda (f)
+      ((delta (e r k)
+              ((meaning 'f r (lambda (v) v)) k)))))
+)"""
+
+  def all = s"""(begin
+(EM (EM $reify_env))
+(EM $delta)
+$call_cc
+)"""
+
+  def go(c: Boolean) = {
+    def cev(s: String) = ev(s.replace("(lambda", "(clambda"))
+    def assertEv(v: Value)(s: String) = {
+      assertResult(v){ev(s)}
+      assertResult(v){cev(s)}
+    }
+    if (c) cev(all) else ev(all)
+    assertEv(I(0)){"(+ 1 (call/cc (lambda (k) 0)))"}
+    assertEv(I(1)){"(+ 1 (call/cc (lambda (k) (k 0))))"}
+  }
+
+  test("delta") {
+    go(false)
+  }
+
+  test("delta (all-compiled)") {
+    go(true)
+  }
+}
