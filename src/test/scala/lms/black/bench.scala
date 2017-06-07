70011298a95ebd195ce7a521e1134b0a7b32c7a5
Nada Amin <namin@alum.mit.edu>
2017-06-07 16:56:13 +0200

    tweaks, but not much diff

--- src/test/scala/lms/black/bench.scala
+++ src/test/scala/lms/black/bench.scala
@@ -18,20 +18,21 @@ object Bench {
   def benchFac() = {
     // Shortcut: using + instead of *, because * is unbound...
     println("fac #,evaluated,compiled,traced evaluated,traced compiled")
-    for (i <- 1 to 10) {
+    for (i <- 0 to 9) {
       clean()
-      ev("(define fac (lambda (n) (if (< n 2) 1 (+ (fac (- n 1)) n))))")
+      ev("(define fac (lambda (n) (if (eq? n 0) 1 (+ (fac (- n 1)) n))))")
       val t1 = bench(ev(s"(fac $i)"))
-      ev("(set! fac (clambda (n) (if (< n 2) 1 (+ (fac (- n 1)) n))))")
+      ev("(set! fac (clambda (n) (if (eq? n 0) 1 (+ (fac (- n 1)) n))))")
       val t2 = bench(ev(s"(fac $i)"))
+      clean()
       ev("(EM (define counter 0))")
       ev("(EM (define old-eval-var eval-var))")
       ev("""(EM (set! eval-var (clambda (e r k)
         (if (eq? e 'n) (set! counter (+ counter 1)) 0)
         (old-eval-var e r k))))""")
-      ev("(set! fac (lambda (n) (if (< n 2) 1 (+ (fac (- n 1)) n))))")
+      ev("(define fac (lambda (n) (if (eq? n 0) 1 (+ (fac (- n 1)) n))))")
       val t3 = bench(ev(s"(fac $i)"))
-      ev("(set! fac (clambda (n) (if (< n 2) 1 (+ (fac (- n 1)) n))))")
+      ev("(set! fac (clambda (n) (if (eq? n 0) 1 (+ (fac (- n 1)) n))))")
       val t4 = bench(ev(s"(fac $i)"))
       println(s"$i,$t1,$t2,$t3,$t4")
     }
